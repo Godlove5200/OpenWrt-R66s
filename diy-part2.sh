58545f2b0d60fe49891828b356dbfc2e2f524609
Godlove5200 <58065776+Godlove5200@users.noreply.github.com>
2022-10-22 10:10:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate   # 定制默认IP
 
 sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d")/$(TZ=UTC+8 date "+%Y.%m.%d")/g" package/lean/default-settings/files/zzz-default-settings
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 
